72635cda9bac5071e0a3db6b17f5dea4e30314ab
liujinghang <liujinghang@shencloudtech.com>
2015-08-25 15:43:24 +0800

    get sub url

--- python/my_pic.py
+++ python/my_pic.py
@@ -2,23 +2,40 @@
 
 import urllib2
 import re
+from urllib2 import HTTPError
 
 mmurl = "http://www.mm8mm8.com/xiurenmote/list_"
 i = 1
 ph = -1
 temp = '''<img src="'''
 
+def downImage(pic_url):
+    localPath = "/home/hang/Downloads/" + str(imgurl) + ".jpg"
+    try:
+        request = urllib2.Request(imglist[imgurl + 1])
+        request.add_header("User-Agent", "fake-client")
+        response = urllib2.urlopen(request)
+        f = file(localPath, "wb")
+        f.write(response.read())
+        f.close()
+    except HTTPError:
+        pass
+
 def getImg(html):
     reg = r'(?<=href=")http://.*?(?=" targe)'
     imgre = re.compile(reg)
-    imglist = re.findall(imgre, html)
+    imglist = re.findall(imgre, html) #get every page url list
     for imgurl in range(0, len(imglist), 2):
         print imglist[imgurl + 1]
+        gir_url = urllib2.urlopen(imglist[imgurl + 1])
+        context = gir_url.read()
+        print context
 
-while i <= 1:
+        
+while i <= 1:      #get every one url
     url  = mmurl + str(i) + ".html"
     i += 1
-    #print url
+    print url
     up = urllib2.urlopen(url)
     cont = up.read()
     getImg(cont)
