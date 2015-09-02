fde16274cc7fadd9ef4a8b88c6b02a3628309af0
hakits <hanglinux@163.com>
2015-09-02 22:10:36 +0800

    use wget for path_pic.py

--- python/path_pic.py
+++ python/path_pic.py
@@ -6,6 +6,7 @@ import re
 from urllib2 import HTTPError
 import urllib
 import time
+import os
 
 def downImage(pic_url, name):
     localPath = "/home/hakits/Downloads/path_pic/" + str(name)
@@ -22,7 +23,7 @@ def downImage(pic_url, name):
 
 mmurl = "http://t1.mm8mm8.com/mm8/tupai/20"
 
-x = range(11, 16) #year y
+x = range(12, 16) #year y
 a = ['%02d' % i for i in range(1, 13)] #month m
 b = ['%03d' % i for i in range(1, 120)] #pic directory  day
 c = range(1, 16) #pic number n
@@ -34,10 +35,12 @@ def getUrl():
                 for n in c:
                     #print mmurl + str(y) + m + '/' + d + '/' + str(n) + ".jpg"
                     real_url =  mmurl + str(y) + m + '/' + d + '/' + str(n) + ".jpg"
-                    pic_name =  str(y) + m + d + str(n) + ".jpg"
-                    #return mmurl + str(y) + m + '/' + d + '/' + str(n) + ".jpg", str(y) + m + d + str(n) + ".jpg"
+                    pic_name =  str(y) + m + d + str(n) + ".jpg "
                     #print real_url, pic_name
-                    downImage(real_url, pic_name) 
+                    wget_url = "wget -O " + pic_name + real_url 
+                    #downImage(real_url, pic_name) 
+                    print wget_url
+                    os.system(wget_url)
                     time.sleep(0.5)
 
 if __name__ == '__main__':
