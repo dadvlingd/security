f479c1259fb423b0d6ea4aee6a3859e3dded44fa
hakits <hanglinux@163.com>
2015-09-11 21:54:33 +0800

    my_pic.py for jandan.com

--- python/my_pic.py
+++ python/my_pic.py
@@ -4,19 +4,19 @@ import urllib2
 import re
 from urllib2 import HTTPError
 import urllib
+import os
 
-mmurl = "http://www.mm8mm8.com/sexy/p"
-i = 1
-j = 4
+mmurl = "http://jandan.net/ooxx/page-"
+#mmurl = "http://www.mm8mm8.com/sexy/p"
 ph = -1
 temp = '''<img src="'''
 ri = 0
 
 def downImage(pic_url, name):
-    localPath = "/home/hakits/Downloads/" + str(name) + ".jpg"
+    localPath = "/home/hakits/Downloads/domm" + name + ".jpg"
     try:
         request = urllib2.Request(pic_url)
-        request.add_header("User-Agent", "fake-client")
+        request.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36")
         response = urllib2.urlopen(request, timeout = 4)
         f = file(localPath, "wb")
         f.write(response.read())
@@ -25,50 +25,42 @@ def downImage(pic_url, name):
         pass
 
 def getImg(html):
-    reg = r'(?<=href=")http://.*?(?=" targe)'
-    i = 0
+    reg = r'(?<=src=")http://.*?.jpg'
     imgre = re.compile(reg)
     imglist = re.findall(imgre, html) #get every page url list
-    for imgurl in range(0, len(imglist), 2):
-        #print imglist[imgurl + 1]
-        gir_url = urllib2.urlopen(imglist[imgurl + 1])
-        context = gir_url.read() #this include all pic link
-        gir_reg = r'(?<=src=").*(?=\d{1,2}.jpg)'
-        gir_com = re.compile(gir_reg)
-        gir_pic_list = re.findall(gir_com, context)
-        for gir_pic in gir_pic_list:
-            print gir_pic
-            '''for i in range(1, 16):
-                global ri
-                #urllib.urlretrieve(gir_pic + str(i) + ".jpg",  str(ri) + ".jpg")
-                downImage(gir_pic + str(i) + ".jpg", ri)
-                ri += 1'''
-        #print context
+    for imgurl in imglist:
+        print imgurl
+        os.system("wget " + imgurl)
+        
+        #downImage(imgurl, imgurl.split('/')[-1])
 
         
-while j <= 8:      #get every one url
-    url  = mmurl + str(j) + ".html"
-    j += 1
-    #print url
-    up = urllib2.urlopen(url)
-    cont = up.read()
-    getImg(cont)
-    '''
-    #print cont
-    head = "<img src="
-    tail = ".jpg"
-    ph = cont.find(head)
-    pj = cont.find(tail, ph + 1)
-    #print cont[ph + len(temp) : pj + len(tail)]
-    ahref = <li><a href=" #there have three '
-    target = "target"
-    pa = cont.find(ahref)
-    pt = cont.find(target, pa)
-    #print cont[pa + len(ahref) : pt - 2]
-    moduleurl = cont[pa + len(ahref) : pt - 2]
-    mup = urllib2.urlopen(moduleurl)
-    mcont = mup.read()
-    print mcont
-    '''
+if __name__ == '__main__':
+    os.chdir("/home/hakits/Downloads/domm")
+    os.system("rm -rf *")
+    for i in range(1501, 1529):      #get every one url
+        url  = mmurl + str(i) + '#comments'
+        request = urllib2.Request(url)
+        request.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36")
+        response = urllib2.urlopen(request)
+        cont = response.read()
+        getImg(cont)
+        '''
+        #print cont
+        head = "<img src="
+        tail = ".jpg"
+        ph = cont.find(head)
+        pj = cont.find(tail, ph + 1)
+        #print cont[ph + len(temp) : pj + len(tail)]
+        ahref = <li><a href=" #there have three '
+        target = "target"
+        pa = cont.find(ahref)
+        pt = cont.find(target, pa)
+        #print cont[pa + len(ahref) : pt - 2]
+        moduleurl = cont[pa + len(ahref) : pt - 2]
+        mup = urllib2.urlopen(moduleurl)
+        mcont = mup.read()
+        print mcont
+        '''
 
 
